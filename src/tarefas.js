97c9d402e5f57c6fa38cda15831ad5d28cefd232
Ariel <ariel.carvalho08@aluno.ifce.edu.br>
2026-06-15 07:25:44 -0300

    fix: garante nomenclatura correta do tarefas.js

---
+++ src/tarefas.js
@@ -0,0 +1,21 @@
+const { db } = require("\ ./database");
+
+function adicionarTarefa(titulo) {
+    return new Promise((resolve, reject) => {
+        db.run("INSERT INTO tarefas (titulo) VALUES (?)", [titulo], function(err) {
+            if (err) reject(err);
+            else resolve({id: this.lastID, titulo, concluida: 0});
+        });
+    });
+}
+
+function listarTarefas() {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM tarefas", [], (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        });
+    });
+}
+
+module.exports = { adicionarTarefa, listarTarefas };
